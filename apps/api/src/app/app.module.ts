07d1df1d26450b6d44cf247f113c7c6bbf87c09c
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-17 03:12:31 +0330

    [Feat] create product module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
