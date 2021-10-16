c0fd349ea625dc2d00f69a4f09aea4ac491b3275
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-17 03:11:14 +0330

    [Feat] implement redis cache service

--- apps/api/src/app/core/core.module.ts
+++ apps/api/src/app/core/core.module.ts
@@ -1,4 +1,23 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+import * as redisStore from 'cache-manager-redis-store';
+import { RedisCacheService } from './services';
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: configService.get('CACHE_TTL'),
+        max: configService.get('MAX_ITEM_IN_CACHE'),
+      }),
+    }),
+  ],
+  providers: [RedisCacheService],
+  exports: [RedisCacheService],
+})
 export class CoreModule {}
--- apps/api/src/app/core/services/redis-cache/redis-cache.service.ts
+++ apps/api/src/app/core/services/redis-cache/redis-cache.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 @Injectable()
-export class RedisCacheService {}
+export class RedisCacheService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+
+  async get<T>(key: string): Promise<T> {
+    return await this.cache.get(key);
+  }
+
+  async set<T>(key: string, value: T): Promise<void> {
+    await this.cache.set(key, value, 1000);
+  }
+
+  async reset(): Promise<void> {
+    await this.cache.reset();
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.cache.del(key);
+  }
+}
