9c53251a0f5fce92d61586831ee13f5f609a06ea
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-17 22:29:56 +0330

    [Refactor] remove starter functions

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@angular-nestjs-redis-e-shop/api-interfaces';
 
 @Injectable()
-export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
-  }
-}
+export class AppService {}
