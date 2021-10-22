237b07f83315f2a4b9d2c23d1bc9bfc4a19c7ba0
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-22 22:37:03 +0330

    [Feat] install angular material

--- apps/angular-nestjs-redis-e-shop/src/app/app.module.ts
+++ apps/angular-nestjs-redis-e-shop/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
