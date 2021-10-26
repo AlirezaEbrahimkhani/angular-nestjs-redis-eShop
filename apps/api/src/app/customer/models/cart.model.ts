87572b2617af7306462215500a1a28f4f39e336c
AliKarimi <calirkarimi7@gmail.com>
2021-10-26 19:32:43 +0330

    [FEAT] creating and indexing cart model

---
+++ apps/api/src/app/customer/models/cart.model.ts
@@ -0,0 +1,4 @@
+export interface CartInterface {
+    products: string[];
+  }
+  
--- apps/api/src/app/customer/models/index.ts
+++ apps/api/src/app/customer/models/index.ts
@@ -1,2 +1,4 @@
 export * from './customer.model';
 export * from './cart-status.model';
+export * from './cart.model';
+
