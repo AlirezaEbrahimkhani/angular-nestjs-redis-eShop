ceee62114aa18a33294f99abb2e3e26bb2de1d49
AliKarimi <calirkarimi7@gmail.com>
2021-10-26 23:58:20 +0330

    [FEAT] add get and register customer

--- apps/api/src/app/customer/controllers/customer.controller.ts
+++ apps/api/src/app/customer/controllers/customer.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { DataResponese } from '../../shared';
+import { RegisterCustomerDTO } from '../dtos';
+import { CustomerService } from '../services';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Get()
+  getAllCustomers(): Promise<DataResponese<Object>> {
+    let data = this.customerService.getAllCustomer();
+    return data;
+  }
+
+  @Post()
+  registerNewCustomer(@Body() registerCustomerDTO: RegisterCustomerDTO) {
+    let insertResponse =
+      this.customerService.registerNewCustomer(registerCustomerDTO);
+    return insertResponse;
+  }
+}
