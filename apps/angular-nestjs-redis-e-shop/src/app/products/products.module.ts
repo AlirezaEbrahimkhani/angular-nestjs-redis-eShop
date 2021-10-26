02141796fed962cc872aa71ac0768631d740dbc4
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-27 00:33:00 +0330

    [Feat] create product module

---
+++ apps/angular-nestjs-redis-e-shop/src/app/products/products.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductsRoutingModule } from './products.routing';
+import { ProductsRoutingComponent } from './prodcuts.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { SharedModule } from '../shared/shared.module';
+
+const Material_Modules: any[] = [MatCardModule, MatButtonModule, MatIconModule];
+
+@NgModule({
+  declarations: [
+    ProductsRoutingComponent,
+    ProductListComponent,
+    ProductCardComponent,
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    Material_Modules,
+    SharedModule,
+  ],
+})
+export class ProductsModule {}
