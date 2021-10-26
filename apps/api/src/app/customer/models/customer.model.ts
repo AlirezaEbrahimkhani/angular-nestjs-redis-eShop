380221d5b4c528b0c9ac98eb23ff64d50afc60e0
AliKarimi <calirkarimi7@gmail.com>
2021-10-26 23:59:44 +0330

    [FEAT] add constructor

--- apps/api/src/app/customer/models/customer.model.ts
+++ apps/api/src/app/customer/models/customer.model.ts
@@ -1,6 +1,7 @@
-import { CartStatus } from "./cart-status.model";
+import { CartStatus } from './cart-status.model';
+import { v4 as uuidv4 } from 'uuid';
 
-export interface CustomerInterface {
+export class Customer {
   username: string;
   password: string;
   first_name: string;
@@ -8,6 +9,20 @@ export interface CustomerInterface {
   email: string;
   phone_number: string;
   img_urls: string[];
-  products_in_cart: string[];
+  products_in_cart: string;
   cart_status: CartStatus;
+  constructor(registerCustomerDTO) {
+    this.username = registerCustomerDTO.username ?? '';
+    this.password = registerCustomerDTO.password ?? '';
+    this.first_name = registerCustomerDTO.first_name ?? '';
+    this.last_name = registerCustomerDTO.last_name ?? '';
+    this.email = registerCustomerDTO.email ?? '';
+    this.phone_number = registerCustomerDTO.phone_number ?? '';
+    this.img_urls = registerCustomerDTO.img_urls ?? [];
+    this.products_in_cart = registerCustomerDTO.products_in_cart ?? uuidv4();
+    this.cart_status = registerCustomerDTO.cart_status ?? {
+      status: 'Done',
+      payable_amount: '0$',
+    };
+  }
 }
