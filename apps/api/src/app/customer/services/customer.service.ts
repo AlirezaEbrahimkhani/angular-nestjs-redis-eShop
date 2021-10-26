d7c66837e03980229faa1916e3e72377a9a97fe1
AliKarimi <calirkarimi7@gmail.com>
2021-10-27 00:01:03 +0330

    [FEAT] add get and register customer to service

--- apps/api/src/app/customer/services/customer.service.ts
+++ apps/api/src/app/customer/services/customer.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { RedisCacheService } from '../../core/services';
+import { Customer } from '../models';
+import { DataResponese, ListName } from '../../shared';
+import { RegisterCustomerDTO } from '../dtos';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(private readonly redisCacheService: RedisCacheService) {}
+
+  async getAllCustomer() {
+    let customers: Object = await this.redisCacheService.get<[Customer]>(
+      ListName.CUSTOMERs
+    );
+
+    return new DataResponese<Customer>(customers);
+  }
+
+  async registerNewCustomer(registerCustomerDTO: RegisterCustomerDTO) {
+    let allCustomers: Object = await this._fetchAllCustomers();
+    let usernameExits = this._checkUsernameExistence(
+      allCustomers,
+      registerCustomerDTO.username
+    );
+    if (usernameExits) return new DataResponese([], false, 'Username Exists !');
+
+    allCustomers = {
+      ...allCustomers,
+      [registerCustomerDTO.username]: new Customer(registerCustomerDTO),
+    };
+    this.redisCacheService
+      .set(ListName.CUSTOMERs, allCustomers)
+      .then(() => {
+        return new DataResponese([], true);
+      })
+      .catch((err) => {
+        if (err) return new DataResponese([], false);
+      });
+  }
+
+  private _checkUsernameExistence(allCustomers: Object, username: string) {
+    return Object.keys(allCustomers).includes(username);
+  }
+
+  private async _fetchAllCustomers(): Promise<Object> {
+    let customers: Object = await this.redisCacheService.get(
+      ListName.CUSTOMERs
+    );
+    return customers;
+  }
+}
