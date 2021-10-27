810a88fa994c96ff2385363f9123b6df35e2a6a7
AliKarimi <calirkarimi7@gmail.com>
2021-10-27 22:44:12 +0330

    [FEAT] creating and indexing aut controller

---
+++ apps/api/src/app/auth/controllers/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { DataResponese } from '../../shared';
+import { LoginCustomerDto, RegisterCustomerDTO } from '../dtos';
+import { AuthService } from '../services';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async loginCustomer(@Body() loginCustomerDto: LoginCustomerDto) {
+    const responseData = await this.authService.loginCustomer(loginCustomerDto);
+    return responseData;
+  }
+
+  @Post('/register')
+  registerNewCustomer(@Body() registerCustomerDTO: RegisterCustomerDTO) {
+    let insertResponse =
+      this.authService.registerNewCustomer(registerCustomerDTO);
+    return insertResponse;
+  }
+}
