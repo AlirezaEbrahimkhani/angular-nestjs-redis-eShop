0d4770e8d72fe268090c8385692ac871e9b517b5
AliKarimi <calirkarimi7@gmail.com>
2021-10-27 19:17:46 +0330

    [FEAT] creating and indexing reg-customer-dto

---
+++ apps/api/src/app/auth/dtos/register-customer.dto.ts
@@ -0,0 +1,10 @@
+export interface RegisterCustomerDTO {
+    username: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    img_urls: string[];
+  }
+  
