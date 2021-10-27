41e8aa2c3ffc0b4eb2735bd9a4d38b87920b5e6a
AliKarimi <calirkarimi7@gmail.com>
2021-10-27 19:06:11 +0330

    [FEAT] create auth module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CoreModule, ProductModule , CustomerModule],
+  imports: [CoreModule, ProductModule , CustomerModule , AuthModule],
 })
 export class AppModule {}
