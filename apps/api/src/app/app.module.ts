badf0ec05b06b96a4352d55338a6d0f37f3cf1ab
AliKarimi <calirkarimi7@gmail.com>
2021-10-28 18:43:02 +0330

    [FEAT] create cart module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 import { CoreModule } from './core/core.module';
 import { CustomerModule } from './customer/customer.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CoreModule, ProductModule , CustomerModule , AuthModule],
+  imports: [CoreModule, ProductModule, CustomerModule, AuthModule, CartModule],
 })
 export class AppModule {}
