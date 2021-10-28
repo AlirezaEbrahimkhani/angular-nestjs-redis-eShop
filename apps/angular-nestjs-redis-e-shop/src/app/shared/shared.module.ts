431b5f37cce87ff1d9e27095fb1c224df0193fff
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-28 23:36:42 +0330

    [Feat] create header less directive to remove mat-tab-group

--- apps/angular-nestjs-redis-e-shop/src/app/shared/shared.module.ts
+++ apps/angular-nestjs-redis-e-shop/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderLessTabsDirective } from './directive/header-less-tabs.directive';
 import { ImageSourceDirective } from './directive/image-source.directive';
 
 @NgModule({
-  declarations: [ImageSourceDirective],
+  declarations: [ImageSourceDirective, HeaderLessTabsDirective],
   imports: [CommonModule],
-  exports: [ImageSourceDirective],
+  exports: [ImageSourceDirective, HeaderLessTabsDirective],
 })
 export class SharedModule {}
