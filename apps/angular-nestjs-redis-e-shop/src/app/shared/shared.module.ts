25b041279f4f85846a76739cadb9ec471e4996a0
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-29 19:26:18 +0330

    [Feat] move information renderer component to shared module

--- apps/angular-nestjs-redis-e-shop/src/app/shared/shared.module.ts
+++ apps/angular-nestjs-redis-e-shop/src/app/shared/shared.module.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { InformationRendererComponent } from './components/information-renderer/information-renderer.component';
 import { HeaderLessTabsDirective } from './directive/header-less-tabs.directive';
 import { ImageSourceDirective } from './directive/image-source.directive';
 
 @NgModule({
-  declarations: [ImageSourceDirective, HeaderLessTabsDirective],
+  declarations: [
+    ImageSourceDirective,
+    HeaderLessTabsDirective,
+    InformationRendererComponent,
+  ],
   imports: [CommonModule],
-  exports: [ImageSourceDirective, HeaderLessTabsDirective],
+  exports: [
+    ImageSourceDirective,
+    HeaderLessTabsDirective,
+    InformationRendererComponent,
+  ],
 })
 export class SharedModule {}
