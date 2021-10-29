3fe387b008f6a0fa42b4e17867fa12f99b9780d7
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-10-29 13:12:33 +0330

    [Feat] render product information

--- apps/angular-nestjs-redis-e-shop/src/app/products/components/information-renderer/information-renderer.component.ts
+++ apps/angular-nestjs-redis-e-shop/src/app/products/components/information-renderer/information-renderer.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-information-renderer',
   templateUrl: './information-renderer.component.html',
-  styleUrls: ['./information-renderer.component.scss']
+  styleUrls: ['./information-renderer.component.scss'],
 })
-export class InformationRendererComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class InformationRendererComponent {
+  @Input('information') set info(information: any) {
+    Object.keys(information).forEach((key) => {
+      const valueType = typeof information[key];
+      if (Array.isArray(information[key])) {
+        information = {
+          ...information,
+          [key]: information[key].join(', '),
+        };
+      }
+      switch (valueType) {
+        case 'boolean':
+          information[key] = valueType ? 'Have' : 'Have not';
+          break;
+      }
+    });
+    this._productInformation = information;
+  }
+  get information() {
+    return this._productInformation;
   }
 
+  private _productInformation = {};
 }
